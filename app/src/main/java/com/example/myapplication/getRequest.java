b28c565d31b919a2e9818b4826f186766e95303f
NaufalFiqri <naufal11bale@gmail.com>
2022-12-24 12:53:10 +0800

    finished chatbot

--- app/src/main/java/com/example/myapplication/getRequest.java
+++ app/src/main/java/com/example/myapplication/getRequest.java
@@ -3,7 +3,7 @@ package com.example.myapplication;
 import android.content.Context;
 import android.util.Log;
 
-import com.android.vollet.RequestQueue;
+import com.android.volley.RequestQueue;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
