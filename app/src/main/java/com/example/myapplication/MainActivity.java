9ec3aa075524ca7f5a6e8dc341ac02d3c0c89ee5
NaufalFiqri <naufal11bale@gmail.com>
2022-12-24 13:35:37 +0800

    refurbished codes

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chatbot);
+        setContentView(R.layout.activity_main);
 
         request = new getRequest(this);
 
